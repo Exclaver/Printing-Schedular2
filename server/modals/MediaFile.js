ba499eda2f60d2dbd2899a45cae32d7ee17ebc9f
Exclaver <devansh0403@gmail.com>
2024-05-23 03:21:09 +0530

    return_back

--- server/modals/MediaFile.js
+++ server/modals/MediaFile.js
@@ -1,27 +1,12 @@
-// const mongoose = require("mongoose");
-
-// const mediaFileSchema = new mongoose.Schema({
-//   filename: { type: String, required: true },
-//   filesize: { type: Number, required: true },
-//   path: { type: String, required: true },
-//   visitcount: { type: Number, default: 0 },
-//   username:{type:String, required:true},
-//   PrintType:{type:String, required:true},
-// });
-
-// const MediaFile = mongoose.model("MediaFile", mediaFileSchema);
-
-// module.exports = MediaFile;
-
 const mongoose = require("mongoose");
 
 const mediaFileSchema = new mongoose.Schema({
   filename: { type: String, required: true },
   filesize: { type: Number, required: true },
-  path: { type: String, required: true }, // This will store the URL of the file in Vercel Blob
+  path: { type: String, required: true },
   visitcount: { type: Number, default: 0 },
-  username: { type: String, required: true },
-  PrintType: { type: String, required: true },
+  username:{type:String, required:true},
+  PrintType:{type:String, required:true},
 });
 
 const MediaFile = mongoose.model("MediaFile", mediaFileSchema);
